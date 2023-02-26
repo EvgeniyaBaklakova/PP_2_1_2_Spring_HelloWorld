1193976587c27147f670e0f71aab81d9927eaa89
EvgeniyaBaklakova <121979488+EvgeniyaBaklakova@users.noreply.github.com>
2023-02-26 16:43:40 +0300

    bin comparison

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,21 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
+
+        boolean helloWord = bean==bean2;
+        System.out.println(helloWord);
+
+        Cat catBean = (Cat) applicationContext.getBean("catBean");
+        System.out.println(catBean);
+
+        Cat catBean2 = (Cat) applicationContext.getBean("catBean");
+        System.out.println(catBean2);
+
+        boolean cat = catBean==catBean2;
+        System.out.println(cat);
     }
 }
